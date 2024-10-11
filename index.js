38889130080002d2cf76a54935679d2631d3ef91
Miyosha Clarke <134561119+diamondgyal16@users.noreply.github.com>
2024-10-11 11:48:28 -0400

    Added html, javascript, and css file.

---
+++ index.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container")
+
+for(let i=0; i<16; i++){
+    const row = document.createElement("div")
+    row.classList.add("row")
+    container.appendChild(row)
+
+    for(let j=0; j<16; j++){
+        const square = document.createElement("div")
+        square.classList.add("box")
+        row.appendChild(square)
+        square.addEventListener("mouseenter" , function(){
+            square.classList.toggle("draw")
+        })
+    }
+}
