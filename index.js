340ef158c61ef04a409ccca19c6056c90adc76bc
Miyosha Clarke <134561119+diamondgyal16@users.noreply.github.com>
2024-11-07 19:15:56 -0500

    Changed the color of the squares and added a darkening effect.

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const number = document.createElement("button");
+container.appendChild(number);
+number.textContent = "number";
 
-const number = document.createElement("button")
-container.appendChild(number)
-number.textContent = "number"
-number.addEventListener ("click" , function (){
-    const gridSize = prompt ("enter grid size")
+number.addEventListener ("click" , function() {
+    const gridSize = prompt ("enter grid size");
 
-    createGrid(parseInt (gridSize))
+    createGrid(parseInt (gridSize));
 })
 
-function createGrid (size){
-    const rows = document.querySelectorAll(".row")
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * 255);
+}
 
-    for (let row of rows){
-        row.remove()
+const darken = (color) => {
+    const match = RegExp(/rgba\((\d{1,3}), (\d{1,3}), (\d{1,3}), (\d?\.?\d)/).exec(color);
+    const alpha = parseFloat(match[4]);
+
+    return `rgba(${match[1]}, ${match[2]}, ${match[3]}, ${(alpha === 1) ? alpha : alpha + .1})`;
+}
+
+function createGrid (size) {
+    // remove all existing rows
+    const rows = document.querySelectorAll(".row");
+
+    for (let row of rows) {
+        row.remove();
     }
+    // end
 
-    for(let i=0; i<size; i++){
-        const row = document.createElement("div")
-        row.classList.add("row")
-        container.appendChild(row)
+    for(let i=0; i<size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
     
-        for(let j=0; j<size; j++){
-            const square = document.createElement("div")
-            square.classList.add("box")
-            row.appendChild(square)
-            square.addEventListener("mouseenter" , function(){
-                square.classList.add("draw")
+        for(let j=0; j<size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+            
+            square.addEventListener("mouseenter" , function() {
+                const color = square.style.backgroundColor;
+
+                if(color) {
+                    square.style.backgroundColor = darken(color);
+                } else {
+                    square.style.backgroundColor = `rgba(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()}, .1)`;
+                }
             })
         }
     }
